6da3ad65d9fd17ea31966dc47624c5d02c26b9e6
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-22 19:28:08 +0800

    Finished

--- appeared_ntimes.py
+++ appeared_ntimes.py
@@ -1,7 +1,7 @@
 # Return the count of a given substring from a string
 
-# Pseudocode
 # The sentence
 sentence = "Mish is a nickname of Melissa. Mish is a student in PUP. Mish is a Computer Engineering Student."
-print(sentence)
 # Print the counted number
+count = sentence.count("Mish")
+print("Mish appeared", count, "times")
